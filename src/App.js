c870b66deff271490be1b8c35a702ba0e9628065
Stargazaar <limqinkai@hotmail.com>
2025-09-07 21:31:44 +0800

    changed greeting

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye There!!!
+          QK React App!!!
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
